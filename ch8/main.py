644775c081d7de7e576bc703a4d58ea8e85df5a9
SONG <simpledeveloper.songs@gmail.com>
2021-12-30 12:12:22 +0900

    쿠키를 이용한 저장

---
+++ ch8/main.py
@@ -0,0 +1,59 @@
+# 쿠키를 이용한 저장
+from typing import Optional
+from fastapi import Cookie, FastAPI, Request
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+import json
+
+
+class Memo(BaseModel):
+    title: str
+    body: Optional[str] = None
+
+
+class Post(BaseModel):
+    ip: str
+    t: Optional[BaseModel] = None
+
+
+app = FastAPI()
+
+
+@app.get("/read_memo/")
+async def read_items(user_ip: Optional[str] = Cookie(None)):
+    print("user_ip", user_ip)
+    my_post = []
+    with open("posts.json", "r") as json_file:
+        posts = json.load(json_file)
+        for post in posts:
+            if post['ip'] == user_ip:
+                my_post.append(post)
+
+    return my_post
+
+
+@app.post("/write_memo/")
+def read_memo(memo: Memo, request: Request):
+    content = {"message": "메모를 서버에 저장합니다."}
+    response = JSONResponse(content=content)
+    print(request.client.host)
+    response.set_cookie(key="user_ip", value=request.client.host)
+
+    post = Post(ip=request.client.host, t=memo)
+
+    try:
+        with open("posts.json", "r") as json_file:
+            posts = json.load(json_file)
+    except:
+        posts = []
+    finally:
+        posts.append(post.dict())
+
+    print(posts)
+
+    with open("posts.json", "w") as json_file:
+        j = json.dumps(posts)  # str
+        posts = json.loads(j)  # list[dict]
+        json.dump(posts, json_file)
+
+    return response
