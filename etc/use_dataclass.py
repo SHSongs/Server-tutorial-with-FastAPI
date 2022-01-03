8374dcb2e517e29dad6529f0d25845bc249b3a8c
SONG <simpledeveloper.songs@gmail.com>
2022-01-03 17:25:58 +0900

    Update use_dataclass.py

--- etc/use_dataclass.py
+++ etc/use_dataclass.py
@@ -14,10 +14,23 @@ class Player:
 
 
 player_1 = Player(pos=Point(3, 4), canFly=True)
-
-print("위치: ", player_1.pos.x, player_1.pos.y)
-print("날 수 있나?: ", player_1.canFly)
+print(player_1)  # Player(pos=Point(x=3, y=4), canFly=True)
 
 pos1 = Point(1, 1)
 pos2 = Point(1, 1)
-print(pos1 == pos2)
+print(pos1 == pos2)  # True
+
+from dataclasses import dataclass, field
+from typing import List
+
+
+@dataclass
+class Record:
+    player: Player = Player()
+    foot_print: List[Point] = field(default_factory=list)
+
+
+record = Record()
+record.foot_print.append(Point(1, 1))
+record.foot_print.append(Point(2, 2))
+print(record.foot_print)  # [Point(x=1, y=1), Point(x=2, y=2)]
