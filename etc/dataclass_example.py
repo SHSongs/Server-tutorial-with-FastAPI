43a902d62f975ce1b6b1ee520c6194edc237935c
SONG <simpledeveloper.songs@gmail.com>
2022-01-03 12:26:08 +0900

    use dataclass

--- etc/dataclass_example.py
+++ etc/dataclass_example.py
@@ -1,15 +1,35 @@
+# class Point:
+#     def __init__(self, x: int = 0, y: int = 0) -> None:
+#         self.x = x
+#         self.y = y
+#
+#
+# class Player:
+#     def __init__(
+#             self, pos: Point = Point(0, 0), canFly: bool = False
+#     ) -> None:
+#         self.pos = pos
+#         self.canFly = canFly
+#
+#
+# player_1 = Player(pos=Point(3, 4), canFly=True)
+#
+# print("위치: ", player_1.pos.x, player_1.pos.y)
+# print("날 수 있나?: ", player_1.canFly)
+
+from dataclasses import dataclass
+
+
+@dataclass
 class Point:
-    def __init__(self, x: int = 0, y: int = 0) -> None:
-        self.x = x
-        self.y = y
+    x: int = 0
+    y: int = 0
 
 
+@dataclass
 class Player:
-    def __init__(
-            self, pos: Point = Point(0, 0), canFly: bool = False
-    ) -> None:
-        self.pos = pos
-        self.canFly = canFly
+    pos: Point = Point(0, 0)
+    canFly: bool = False
 
 
 player_1 = Player(pos=Point(3, 4), canFly=True)
